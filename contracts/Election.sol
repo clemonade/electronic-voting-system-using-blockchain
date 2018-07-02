04a8f59227c43e2ae58b5256b0ee9d44d5f672ef
clemonade <clement2296@gmail.com>
2018-07-03 03:03:49 +0800

    Added registration functions for Candidate and Party Added modifiers

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.24;
 import "./Ownable.sol";
 
 contract Election is Ownable {
-    enum ConstituencyType {Federal, State}
+    enum ConstituencyType {FEDERAL, STATE}
     uint totalVotes;
 
     modifier hasNotVoted(string _nonce){
@@ -11,21 +11,37 @@ contract Election is Ownable {
         _;
     }
 
+    modifier isValidConstituencyType(uint _constituencyType){
+        require(uint(ConstituencyType.STATE) >= _constituencyType);
+        _;
+    }
+
+    modifier isValidParty (uint _party){
+        require(parties.length > _party);
+        _;
+    }
+
+    modifier isValidCandidate(uint _candidateId){
+        require(candidates.length > _candidateId);
+        _;
+    }
+
     struct Constituency {
         uint numVotes;
-        string constituency;
         ConstituencyType constituencyType;
         Candidate[] candidates;
     }
 
     struct Party {
-        string abbreviation;
         string name;
+        string abbreviation;
     }
 
     struct Candidate {
-        uint numVotes;
         string name;
+        uint numVotesFederal;
+        uint numVotesState;
+        bool validCandidate;
     }
 
     struct Voter {
@@ -34,24 +50,56 @@ contract Election is Ownable {
         uint votedForState;
     }
 
-    //Candidate[] candidates;
-    //Party[] parties;
+    Candidate[] candidates;
+    Party[] parties;
+    uint[] candidateToParty;
 
     mapping(string => Voter) votes;
-    mapping(uint => Party) candidateToParty;
+    mapping(string => Constituency) constituencies;
 
-    constructor(){
+    constructor() public {
         totalVotes = 0;
     }
 
-    function registerCandidate(string _name, ConstituencyType constituency, string constituency, string party){
-
+    function registerParty(string _partyName, string _partyAbbreviation) onlyOwner() {
+        Party memory party = Party(_partyName, _partyAbbreviation);
+        parties.push(party);
     }
 
-    function vote(string _nonce, uint _votedForFederal, uint _votedForState) hasNotVoted(_nonce) {
+    function registerCandidate(
+        uint _constituencyType,
+        string _constituencyCode,
+        string _candidateName,
+        uint _party) onlyOwner() isValidConstituencyType(_constituencyType) isValidParty(_party) {
+        uint candidateId = candidates.length;
+        Candidate memory candidate = Candidate(_candidateName, 0, 0, true);
+        candidateToParty[candidateId] = _party;
+        candidates.push(candidate);
+
+        if (constituencies[_constituencyCode].candidates.length == 0) {
+            initiateConstituency(_constituencyType, _constituencyCode);
+        }
+
+        constituencies[_constituencyCode].candidates.push(candidate);
+    }
+
+    function initiateConstituency(uint _constituencyType, string _constituencyCode) isValidConstituencyType(_constituencyType) {
+        constituencies[_constituencyCode].numVotes = 0;
+        constituencies[_constituencyCode].constituencyType = ConstituencyType(_constituencyType);
+    }
+
+    function invalidateCandidate(uint _candidateId) onlyOwner() isValidCandidate(_candidateId) {
+        candidates[_candidateId].validCandidate = false;
+    }
+
+    function vote(string _nonce, uint _votedForFederal, uint _votedForState) onlyOwner() hasNotVoted(_nonce) {
         totalVotes++;
         votes[_nonce].hasVoted = true;
         votes[_nonce].votedForFederal = _votedForFederal;
         votes[_nonce].votedForState = _votedForState;
     }
+
+    /*function getTest() view returns(){
+
+    }*/
 }
